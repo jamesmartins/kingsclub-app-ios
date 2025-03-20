67f573f6f5f33a25874f5b3fa7483d6e30be9e13
Duane de Moura Silva <duanemsilva@gmail.com>
2025-03-20 17:02:31 -0300

    layout Inicial

---
+++ KingsClub/KingsClub/KingsClubApp.swift
@@ -0,0 +1,18 @@
+//
+//  KingsClubApp.swift
+//  KingsClub
+//
+//  Created by Duane de Moura Silva on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct KingsClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+            LoginView()
+        }
+    }
+}
